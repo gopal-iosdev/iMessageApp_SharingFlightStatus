ef4ae0e1c890a2e48a945d24ca666f806d717643
Gopal Rao Gurram <Gopal@gavins-macbook-pro-old.local>
2017-03-09 10:32:22 -0600

    Initial Commit

---
+++ MessagesExtension/MessagesViewController.h
@@ -0,0 +1,13 @@
+//
+//  MessagesViewController.h
+//  MessagesExtension
+//
+//  Created by Gopal Rao Gurram on 3/9/17.
+//  Copyright © 2017 Gopal Rao Gurram. All rights reserved.
+//
+
+#import <Messages/Messages.h>
+
+@interface MessagesViewController : MSMessagesAppViewController
+
+@end
